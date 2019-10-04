92e9f27433f17e47814082c25999fa51e1ba1562
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-10-04 14:25:03 -0500

    init

---
+++ env.dev.js
@@ -0,0 +1,11 @@
+/**
+ * Environment configuration for development
+ *
+ * Replace environment.js with this file in the development version
+ */
+
+var __env = {}
+
+__env.apiUrl = "http://kind.cs.uiowa.edu:8080/cvcservices-dev/";
+
+
---
+++ environment.js
@@ -0,0 +1,16 @@
+/**
+ * Environment configuration
+ */
+
+var __env = {}
+
+// production api Url. No need to change this when deploying the development branch
+__env.apiUrl = "http://kind.cs.uiowa.edu:8080/cvcservices/";
+
+// Replace this file with env.dev.js to set development configuration
+
+// remote development api Url
+//__env.apiUrl = "http://kind.cs.uiowa.edu:8080/cvcservices-dev/";
+
+// local development api Url
+//__env.apiUrl = "http://localhost:8080/";
